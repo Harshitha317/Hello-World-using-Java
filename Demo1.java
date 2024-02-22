72c6fdcf75b1f65914bf5753e564a15ce5ce4205
Harshitha317 <160821978+Harshitha317@users.noreply.github.com>
2024-02-22 11:37:54 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,9 @@
+//This program is used to print Hello World
+import java.io.*;
+class Demo1
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hello"+" "+"World!!");
+    }//main
+}//Demo1
